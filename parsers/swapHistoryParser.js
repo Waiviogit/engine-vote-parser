d634eeca1af0f17f2b6e5eaa7a15968aada0173d
unknown <Alishern@wizardsdev@gmail.com>
2021-12-07 21:57:09 +0600

    swap

--- parsers/swapHistoryParser.js
+++ parsers/swapHistoryParser.js
@@ -17,12 +17,6 @@ exports.parse = async (transaction, blockNumber, timestamp) => {
   const symbolOut = _.get(symbols, 'data.symbolOut');
   const symbolIn = _.get(symbols, 'data.symbolIn');
 
-  /*
-  *    check to include only WAIV transactions
-  * */
-
-  if (symbolOut !== 'WAIV' && symbolIn !== 'WAIV') return;
-
   if (!swapTo || !swapFrom || !symbols) return;
 
   const data = {
