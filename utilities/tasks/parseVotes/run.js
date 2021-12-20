36800fc9ed1647bb6977c46c8b85d72662413236
Boris Andrzhiievskyi <borees2@gmail.com>
2021-12-20 17:42:22 +0200

    git 3373 add tasks for rewards && votes

---
+++ utilities/tasks/parseVotes/run.js
@@ -0,0 +1,16 @@
+const api = require('api/hiveEngine');
+const { runCustomStream } = require('utilities/helpers/customStreamHelper');
+const parseVotes = require('./parseVotes');
+
+(async () => {
+  await runCustomStream(
+    {
+      key: process.argv[2],
+      startBlock: +process.argv[3],
+      finishBlock: +process.argv[4],
+      callback: parseVotes.parse,
+      api,
+    },
+  );
+  process.exit();
+})();
---
+++ utilities/tasks/setAccountHistory/setRewardsHistory.js
@@ -0,0 +1,16 @@
+const api = require('api/hiveEngine');
+const { runCustomStream } = require('utilities/helpers/customStreamHelper');
+const setRewardsHelper = require('./setRewardsHelper');
+
+(async () => {
+  await runCustomStream(
+    {
+      key: process.argv[2],
+      startBlock: +process.argv[3],
+      finishBlock: +process.argv[4],
+      callback: setRewardsHelper.parse,
+      api,
+    },
+  );
+  process.exit();
+})();
