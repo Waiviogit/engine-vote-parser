1f7aff7b23f90f181988e979ed9f38c0d2fbd445
Boris Andrzhiievskyi <borees2@gmail.com>
2021-12-21 10:47:59 +0200

    remove stream

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ const express = require('express');
 const logger = require('morgan');
 require('jobs');
 
-const { runStream, runEngineStream } = require('processor/processor');
+const { runEngineStream } = require('processor/processor');
 
 const app = express();
 
 app.use(logger('dev'));
-//
-// runStream().catch((err) => {
-//   console.log(err);
-//   process.exit(1);
-// });
 
 runEngineStream().catch((err) => {
   console.log(err);
