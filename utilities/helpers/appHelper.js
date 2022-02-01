66ff1bf48da542043fdf48d5bc9b545381149745
unknown <Alishern@wizardsdev@gmail.com>
2022-02-01 18:34:48 +0600

    fix

--- utilities/helpers/appHelper.js
+++ utilities/helpers/appHelper.js
@@ -2,7 +2,7 @@ const App = require('models/AppModel');
 const config = require('config');
 
 exports.getBlackListUsers = async () => {
-  const { result: app } = await App.findOne({ host: 'waivio.com' });
+  const { result: app } = await App.findOne({ host: config.appHost });
   if (!app) return { error: { message: 'App not found!' } };
   return { users: app.black_list_users, referralsData: app.referralsData };
 };
