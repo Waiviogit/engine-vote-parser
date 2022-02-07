ab88c9af942dac065c6c29c8ed5b0fa779b2afd5
unknown <Alishern@wizardsdev@gmail.com>
2022-02-07 18:48:54 +0600

    fix

--- parsers/tokensParser.js
+++ parsers/tokensParser.js
@@ -7,40 +7,42 @@ exports.parse = async (transaction, blockNumber) => {
   const payload = parseJson(_.get(transaction, 'payload'));
   const action = _.get(transaction, 'action');
   if (_.isEmpty(payload)) return;
-  const delegateTo = action === ENGINE_CONTRACT_ACTIONS.DELEGATE ? _.get(payload, 'to') : _.get(payload, 'from');
+
   switch (action) {
     case ENGINE_CONTRACT_ACTIONS.DELEGATE:
     case ENGINE_CONTRACT_ACTIONS.TRANSFER:
+      await sendNotification({
+        id: action,
+        block: blockNumber,
+        data: {
+          from: _.get(transaction, 'sender'),
+          to: _.get(payload, 'to'),
+          amount: `${_.get(payload, 'quantity')} ${_.get(payload, 'symbol')}`,
+          memo: _.get(payload, 'memo') ? _.get(payload, 'memo') : '',
+        },
+      });
+      break;
     case ENGINE_CONTRACT_ACTIONS.UNDELEGATE:
       await sendNotification({
         id: action,
         block: blockNumber,
         data: {
           from: _.get(transaction, 'sender'),
-          to: delegateTo,
+          to: _.get(payload, 'from'),
           amount: `${_.get(payload, 'quantity')} ${_.get(payload, 'symbol')}`,
           memo: _.get(payload, 'memo') ? _.get(payload, 'memo') : '',
         },
       });
       break;
     case ENGINE_CONTRACT_ACTIONS.STAKE:
-      await sendNotification({
-        id: ENGINE_CONTRACT_ACTIONS.TRANSFER_TO_VESTING,
-        block: blockNumber,
-        data: {
-          amount: `${_.get(payload, 'quantity')} ${_.get(payload, 'symbol')}`,
-          from: _.get(payload, 'to'),
-          to: _.get(payload, 'to'),
-        },
-      });
-      break;
     case ENGINE_CONTRACT_ACTIONS.UNSTAKE:
       await sendNotification({
         id: action,
         block: blockNumber,
         data: {
           amount: `${_.get(payload, 'quantity')} ${_.get(payload, 'symbol')}`,
-          account: _.get(payload, 'to'),
+          to: _.get(payload, 'to'),
+          from: _.get(payload, 'to'),
         },
       });
       break;
