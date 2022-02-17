2136fc056d8dced7b51fb2b7bfff4c3b309be82d
Boris Andrzhiievskyi <borees2@gmail.com>
2022-02-17 10:37:22 +0200

    add update constants

--- constants/bookBot.js
+++ constants/bookBot.js
@@ -7,16 +7,8 @@ const BOOK_WAIV = {
   percentSwap: 0.2,
 };
 
-const BOOK_BEE2 = {
-  account: 'dhedge-drips',
-  key: process.env.BOOK_BOT_WAIV_KEY,
-  symbol: 'WAIV',
-  tokenPair: 'SWAP.HIVE:WAIV',
-  tradePercent: 0.2,
-};
-
 exports.POOL_FEE = 0.9975;
 
 exports.BOOK_BOTS = [
-  BOOK_WAIV, BOOK_BEE2,
+  BOOK_WAIV,
 ];
