cccee5d5fde6eac922b71cb891198c30fee2b0e6
MashaYakovenko <mariiay@wizardsdev.com>
2022-04-13 16:26:14 +0300

    not to lose

--- constants/pyramidalBot.js
+++ constants/pyramidalBot.js
@@ -1,5 +1,8 @@
 const BEE_HBD_HIVE = {
   tokenPairs: ['SWAP.HIVE:BEE', 'SWAP.HIVE:SWAP.HBD', 'BEE:SWAP.HBD'],
+  // ?? tokenPairs: ['SWAP.HIVE:BEE', 'BEE:SWAP.HBD'],
+  stableTokens: ['SWAP.HIVE', 'SWAP.HBD'],
+  stablePair: 'SWAP.HIVE:SWAP.HBD',
 };
 
 exports.PYRAMIDAL_BOTS = [
--- parsers/poolsParser.js
+++ parsers/poolsParser.js
@@ -1,10 +1,8 @@
 const _ = require('lodash');
 const {
   ENGINE_CONTRACTS,
-  MARKET_CONTRACT_BOOKBOT_EVENT,
 } = require('../constants/hiveEngine');
 const jsonHelper = require('../utilities/helpers/jsonHelper');
-const { BOOK_BOTS } = require('../constants/bookBot');
 const { PYRAMIDAL_BOTS } = require('../constants/pyramidalBot');
 
 exports.parse = async ({ transactions }) => {
@@ -12,36 +10,41 @@ exports.parse = async ({ transactions }) => {
 //  if (process.env.NODE_ENV !== 'staging') return;
 
   // возможно, маркет не нужен
-  const { market, marketPool } = _.reduce(transactions, (acc, transaction) => {
-    const marketCondition = transaction.contract === ENGINE_CONTRACTS.MARKET
-      && _.includes(MARKET_CONTRACT_BOOKBOT_EVENT, transaction.action);
+  const { marketPool } = _.reduce(transactions, (acc, transaction) => {
     const marketPoolCondition = transaction.contract === ENGINE_CONTRACTS.MARKETPOOLS;
-
-    if (marketCondition) acc.market.push(transaction);
     if (marketPoolCondition) acc.marketPool.push(transaction);
-    return acc;
-  }, { market: [], marketPool: [] });
 
- // console.log('market', market);
+    return acc;
+  }, { marketPool: [] });
+
   console.log('marketPool', marketPool);
 
-  const usualEvent = [];
-  handleSwapEvents({ marketPool, usualEvent });
+  const events = [];
+  handleSwapEvents({ marketPool, events });
 };
 
-const handleSwapEvents = ({ marketPool, usualEvent }) => {
+const handleSwapEvents = ({ marketPool, events }) => {
   for (const marketPoolElement of marketPool) {
     const payload = jsonHelper.parseJson(_.get(marketPoolElement, 'payload'));
     const logs = jsonHelper.parseJson(_.get(marketPoolElement, 'logs'));
     if (_.isEmpty(logs) || _.has(logs, 'errors')) continue;
-    const hasBookPools = _.find(_.map(PYRAMIDAL_BOTS, 'tokenPairs'), _.get(payload, 'tokenPair'));
-    console.log('hasBookPools', hasBookPools);
-    if (!hasBookPools) continue;
-    // const symbols = _.find(logs.events, (e) => e.event === 'swapTokens');
-    //
-    // const symbolOut = _.get(symbols, 'data.symbolOut');
-    // const symbolIn = _.get(symbols, 'data.symbolIn');
-    // const bookSymbol = symbolOut === 'SWAP.HIVE' ? symbolIn : symbolOut;
-    // usualEvent.push({ symbol: bookSymbol });
+
+    const imbalancedPool = _.find(_.flatten((_.map(PYRAMIDAL_BOTS, 'tokenPairs'))),
+      (pair) => _.includes(pair, _.get(payload, 'tokenPair')));
+    console.log('imbalancedPool', imbalancedPool);
+    if (!imbalancedPool) continue;
+
+
+     // то это тригер для бота! нужно ли здесь смотреть аут и ин? или достаточно тут же дергать бота?
+    // номер блока для такого тригера с одним из пулов 16484360 (как пример)
+    const symbols = _.find(logs.events, (e) => e.event === 'swapTokens');
+
+    const symbolOut = _.get(symbols, 'data.symbolOut');
+    console.log('symbolOut', symbolOut);
+    const symbolIn = _.get(symbols, 'data.symbolIn');
+    console.log('symbolIn', symbolIn);
+    // если аут здесь не БИ, то пойти и посмотреть не купить ли в этом пуле БИ (чтоб продать в другом)
+    const bookSymbol = !_.includes(_.map(PYRAMIDAL_BOTS, 'imbalancedToken'), symbolOut) ? symbolIn : symbolOut;
+    events.push({ symbol: bookSymbol });
   }
 };
