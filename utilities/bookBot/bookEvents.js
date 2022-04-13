ac8313832e5c9cc467c8998ee01c5575073d3778
fesmofet <borees2@gmail.com>
2022-04-13 16:32:48 +0300

    jobs refactor

--- utilities/bookBot/bookEvents.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-const { BOOK_EMITTER_EVENTS } = require('constants/bookBot');
-const { sendBotRCNotification } = require('../telegramApi/telegramApiRequsts');
-
-const bookEmitter = new EventEmitter();
-
-bookEmitter.once(BOOK_EMITTER_EVENTS.RC, sendBotRCNotification);
-
-module.exports = bookEmitter;
