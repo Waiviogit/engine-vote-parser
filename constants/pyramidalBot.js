aa4e84b06bde0e2dce1e5de842e62591d5dd9a8e
MashaYakovenko <mariiay@wizardsdev.com>
2022-04-22 11:27:23 +0300

    slippage change and 2 more pyramids

--- constants/pyramidalBot.js
+++ constants/pyramidalBot.js
@@ -11,8 +11,36 @@ const BEE_HBD_HIVE = {
   approachCoefficient: 0.99,
 };
 
+const CENT_HBD_HIVE = {
+  account: process.env.TRI_BOT_ACCOUNT,
+  key: process.env.TRI_BOT_KEY,
+  tokenPairs: ['SWAP.HIVE:CENT', 'SWAP.HBD:CENT', 'SWAP.HIVE:SWAP.HBD'],
+  stableTokens: ['SWAP.HIVE', 'SWAP.HBD'],
+  stablePair: 'SWAP.HIVE:SWAP.HBD',
+  tokenSymbol: 'CENT',
+  lowestAmountOutBound: 0.1,
+  startIncomeDifference: 0,
+  tokenPrecision: 8,
+  approachCoefficient: 0.99,
+};
+
+const BUSD_HBD_HIVE = {
+  account: process.env.TRI_BOT_ACCOUNT,
+  key: process.env.TRI_BOT_KEY,
+  tokenPairs: ['SWAP.HIVE:SWAP.BUSD', 'SWAP.HBD:SWAP.BUSD', 'SWAP.HIVE:SWAP.HBD'],
+  stableTokens: ['SWAP.HIVE', 'SWAP.HBD'],
+  stablePair: 'SWAP.HIVE:SWAP.HBD',
+  tokenSymbol: 'SWAP.BUSD',
+  lowestAmountOutBound: 0.1,
+  startIncomeDifference: 0,
+  tokenPrecision: 8,
+  approachCoefficient: 0.99,
+};
+
 exports.PYRAMIDAL_BOTS = [
   BEE_HBD_HIVE,
+  CENT_HBD_HIVE,
+  BUSD_HBD_HIVE,
 ];
 
-exports.SLIPPAGE = 0.005;
+exports.SLIPPAGE = 0.0002;
