f35d25f7a7d416c0ab8a12cf5375da5baf279812
fesmofet <borees2@gmail.com>
2023-07-10 14:09:23 +0300

    5569_issue

--- constants/appData.js
+++ constants/appData.js
@@ -105,8 +105,8 @@ const HIVE_MIND_NODES = [
 ];
 
 const HIVE_ENGINE_NODES = [
-  // 'https://api.hive-engine.com/rpc', // Germany
-  // 'https://api2.hive-engine.com/rpc', // Finland
+  'https://api.hive-engine.com/rpc', // Germany
+  'https://api2.hive-engine.com/rpc', // Finland
   'https://herpc.dtools.dev',
   'https://us.engine.rishipanthee.com',
   'https://ha.herpc.dtools.dev',
