1a0c384c95f72554199ca3d6035a61c7f4eb67ab
fesmofet <borees2@gmail.com>
2025-01-09 16:51:47 +0200

    grey-list-update

--- parsers/marketParser.js
+++ parsers/marketParser.js
@@ -1,7 +1,8 @@
 const { TOKEN_WAIV } = require('constants/hiveEngine');
 const redisSetter = require('utilities/redis/redisSetter');
-const { GREY_LIST_JOB_KEY } = require('constants/common');
-const jsonHelper = require('../utilities/helpers/jsonHelper');
+const redisGetter = require('utilities/redis/redisGetter');
+const { GREY_LIST_JOB_KEY, GREY_LIST_KEY } = require('constants/common');
+const jsonHelper = require('utilities/helpers/jsonHelper');
 
 const parse = async (transaction, blockNumber, timestamp) => {
   if (process.env.NODE_ENV !== 'production') return;
@@ -9,6 +10,12 @@ const parse = async (transaction, blockNumber, timestamp) => {
 
   const payload = jsonHelper.parseJson(transaction.payload, null);
   if (TOKEN_WAIV.SYMBOL === payload?.symbol) {
+    const inGreyList = !!await redisGetter.sismember({
+      key: GREY_LIST_KEY,
+      member: transaction.sender,
+    });
+    if (inGreyList) return;
+
     await redisSetter.sadd(GREY_LIST_JOB_KEY, transaction.sender);
   }
 };
